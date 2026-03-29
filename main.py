7977e4948689acbe25ab48bd1a46d7dd43ebd20c
Void <me5634524@gmail.com>
2026-03-29 14:02:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #
-import pygame 
+import pygame
+from os.path import join
+import random
 
 # General setup
 pygame.init()
@@ -12,9 +14,15 @@ pygame.display.set_caption('Space ship game')
 surf = pygame.Surface((100, 200))
 surf.fill('BlueViolet')
 x = 100
-y = 150
 
 # Import an image
+
+player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
+player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
+star_positions = [(random.randint(0, WINDOW_WIDTH), random.randint(0, WINDOW_HEIGHT)) for i in range (20)]
+# Stars
+
 
 while running:
     # event loop
@@ -24,8 +32,11 @@ while running:
 
     # draw the game
     screen.fill('DarkGrey')
-    x += 0.1
-    screen.blit(surf, (x, y))
+    for pos in star_positions:
+        screen.blit(star_surf, pos)
+    if player_rect.right < WINDOW_WIDTH:
+        player_rect.left += 0.2
+    screen.blit(player_surf, player_rect)
     pygame.display.update()
 
 pygame.quit()
